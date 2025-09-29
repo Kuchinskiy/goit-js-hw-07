d85c3a8136b5e2fffebac10bbc01fc6235e57713
Serhii Kuchynskyi <domarch388640@gmail.com>
2025-09-29 16:51:44 +0300

    task complete

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,16 @@
 'use strict';
 
-const categories = document.querySelector();
+const categoriesList = document.querySelector('#categories');
+const categoryItems = categoriesList.querySelectorAll('.item');
+
+console.log(`Number of categories: ${categoryItems.length}`);
+
+
+
+categoryItems.forEach(item => {
+	const categoryTitle = item.querySelector('h2').textContent;
+	const itemElements = item.querySelectorAll('li');
+
+	console.log(`Category: ${categoryTitle}`);
+	console.log(`Elements: ${itemElements.length}`);
+});
